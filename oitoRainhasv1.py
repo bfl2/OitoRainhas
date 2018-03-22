43e4d476354197004f49b4d8c71e3932619d4e11
Bruno Filizola Leal <bfl2@cin.ufpe.br>
2018-03-22 13:44:03 -0300

    adicionada funcao para conversa de pops inteiras em binarias

--- oitoRainhasv1.py
+++ oitoRainhasv1.py
@@ -3,6 +3,8 @@ import random;
 import numpy;
 from numpy import sum;
 
+# ------# Variaveis Globais
+numAvalFitness = 0;  # condicao de parada, 10.000 avaliacoes de fitness
 
 def bin3Gen(num):
     bin3 = [];
@@ -34,11 +36,11 @@ def binToInt(bin):
     return res;
 
 
-def binToIntIndiv(binI):
-    indiv = [];
-    for e in binI:
-        indiv.append(binToInt(e));
-    return indiv;
+def binToIntIndiv(indivB):
+    indivI = [];
+    for e in indivB:
+        indivI.append(binToInt(e));
+    return indivI;
 
 
 def binToIntPop(popB):
@@ -46,6 +48,19 @@ def binToIntPop(popB):
     for e in popB:
         popI.append(binToIntIndiv(e));
     return popI;
+
+def intToBinIndiv(indivI):
+    indivB =[];
+    for e in indivI:
+        indivB.append(bin3Gen(e));
+
+    return indivB;
+
+def intToBinPop(popI):
+    popB = [];
+    for e in popI:
+        popB.append(intToBinIndiv(e));
+    return popB;
 
 
 def populate(popSize, seed):
@@ -72,12 +87,9 @@ def displayPop(popI):
 
     return;
 
-
-# ------# Variaveis Globais
-numFitnessAval = 0;  # condicao de parada, 10.000 avaliacoes de fitness
-
-
 def fitness(indiv):
+    global numAvalFitness;
+    numAvalFitness+= 1;
     colisoes = 0;
     indivx = indiv;
     flagM1 = [0] * 15;
@@ -98,36 +110,73 @@ def fitness(indiv):
     fit = 1 / (1 + colisoes / 2);
     return fit;
 
-def evolve(pop): #funcao que roda a iteracao do algoritmo evolutivo, chama funcoes de selecao de pais, crossover e mutacao
-    parents = selectParents(pop);
+def geraFilhos(popI): #funcao que roda a iteracao do algoritmo evolutivo, chama funcoes de selecao de pais, crossover e mutacao
+    filhos = [];
+    parents = selectParents(popI);
+    filhosB = intToBinPop(filhos); ## fazer crossover e mutacao com a representacao binaria
+    ##filhosB = crossOver(parents); ## A implementar
+    ##filhosB = mutation(filhos);   ## A implementar
 
-    return parents;
+
+
+    return filhos;
 
 def selectParents(iniPop):
     parents =[];
     random.shuffle(iniPop);
-    print("=========");
-
     for e in iniPop:
         if(len(parents)<5):
             parents.append(e);
     parents = sorted(parents, key=fitness);
-    parents = parents[3:]; #selecionando os dois melhores
-    for e in parents:
-        print("fit*: ", fitness(e));
+    parents = parents[3:]; #selecionando os dois melhores invididuos do conjunto de 5 aleatorios.
+
     return parents;
 
-def main():
+def crossOver(parents): #### A ser implementado ####
+    crossChance = 0.9;
+    filhos =[];
+
+    return filhos;
+
+def mutation(filhos): #### A ser implementado ####
+    mutationChance =0.1;
+
+    return filhos;
+
+def selecaoPop(popI, filhos):#### A ser implementado ####
+    ## funcao deve acomodar filhos na populacao e retirar os piores individuos ate que restem 100 individuos na populacao
+    popSel =[];##retornar esta
+
+    return popI;
+
+def main(): ##popB -> populacao no formato binario, popI -> populacao no formato inteiro
+    ##Inicializacao da populacao
+    global numAvalFitness;
+    condicaoParada = False;
     populationSize = 10;
+    numGeracoes = 0;
     seed = seedGen();
     populationB = populate(populationSize, seed);
     populationI = binToIntPop(populationB);
-    populationI = evolve(populationI);
-    print("----");
-    displayPopBin(populationB);
+
     displayPop(populationI);
+
+
+    #Etapa evolutiva
+    while (condicaoParada != True and False):
+        filhos = geraFilhos(populationI);
+        populationI = selecaoPop(populationI,filhos);
+        numGeracoes+=1;
+
+        if(numAvalFitness>100):
+            condicaoParada = True;
+        displayPop(populationI);
+        print("geracao: ", numGeracoes);
+
     return;
 
 
+
+### funcoes de conversao de inteiro para binario e binario para inteiro implementadas mais em cima
 main();
 
